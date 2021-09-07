187a81e588309f84464cb47e217aad0a3a8d551c
RiyaB <RiyaB@RIYAB-LPT>
2021-09-07 11:31:42 -0400

    Add project files.

---
+++ DIS_Assignmnet1_Fall_2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIS_Assignmnet1_Fall_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
